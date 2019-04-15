0ee38c130b0027be1adedb0aa8e67e9ece742901
Spencer Ang <boomerandzapper@gmail.com>
2019-04-15 16:36:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function execute() {
   var lines = document.getElementById("text").value.split('\n');
-  var newlines = "";
-  console.log(lines.length)
-  for (var i = 0; i < lines.length; i++) {
-    if (lines[i].trim().length) {
-      newlines+=("\"" +lines[i].trim()+"\"")
-        if ((i+1) !== lines.length) {
-          newlines+=", ";
-        }
-    }
-  }
-  document.getElementById("text").value = newlines
+  var newlines = `			<Table
+				rows="{ model: 'Dashboard', path:'contactNameData', templateShareable: false }"
+				title="Repo Info"
+				visibleRowCount="7">
+				<columns>
+					<Column
+						id="name"
+						width="11rem"
+						sortProperty="repoId">
+						<m:Label text="Repo Id" />
+						<template>
+							<m:Text text="{Dashboard>repoId}" wrapping="false" />
+						</template>
+					</Column>
+				</columns>
+			</Table>`;
+//   console.log(lines.length)
+//   for (var i = 0; i < lines.length; i++) {
+//     if (lines[i].trim().length) {
+//       newlines+=("\"" +lines[i].trim()+"\"")
+//         if ((i+1) !== lines.length) {
+//           newlines+=", ";
+//         }
+//     }
+//   }
+  document.getElementById("text3").value = newlines
 }
   
 
+
 function copy() {
-  var copyTextarea = document.querySelector('#text');
+  var copyTextarea = document.querySelector('#text3');
   copyTextarea.select();
 
   try {
