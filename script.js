66b410580c46b2c7f3259ca6a65ef3694e28bd07
Spencer Ang <boomerandzapper@gmail.com>
2019-04-16 14:01:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function execute() {
   var newTable = `			<t:Table
-				rows=rowPlaceholder
-				class="tewTable"
-				noData="No data"
+				rows= rowPlaceholder
+				class= "tewTable"
+				noData= noDataPlaceholder
 				visibleRowCount="20">
 				<t:columns>
 				columnPlaceholder
@@ -12,67 +12,88 @@ function execute() {
 var newColumn = `					<t:Column
 						attPlaceholder
 						sortProperty="sortPropertyPlaceholder">
-						<m:Label text="textPropertyPlaceholder" />
+						textPropertyPlaceholder
 						<t:template>
 							templatePlaceholder
 						</t:template>
 					</t:Column>
 `;
-  
+  	var noData = "No Data";
 	var oldTable = document.getElementById("text").value;
 	var items = oldTable.substring(
-		    oldTable.lastIndexOf("items=") + 6, 
-		    oldTable.indexOf(">")
+		    oldTable.indexOf("items=") + 6, 
+		    oldTable.substring(oldTable.indexOf("items=")).indexOf(`" >`)+ 1 + oldTable.indexOf("items=")
 		).trim();
+
+	if (oldTable.indexOf("noDataText=") > 0) {
+		noData = oldTable.substring(
+		    oldTable.indexOf("noDataText=") + 11, 
+		    oldTable.substring(oldTable.indexOf('noDataText="')+13).indexOf(`"`)+ oldTable.indexOf('noDataText="')+13
+		).trim() + '"';
+	}
+
 	newTable = newTable.replace("rowPlaceholder", items);
 	var columnHeaders = oldTable.substring(
 	    oldTable.lastIndexOf("<columns>") + 9, 
 	    oldTable.indexOf("</columns>")
 	).trim();
+
 	var cells = oldTable.substring(
 	    oldTable.lastIndexOf("<cells>") + 7, 
 	    oldTable.indexOf("</cells>")
 		).trim();
+
 	var headerArray = columnHeaders.split(`<Column`);
+
 	headerArray.map(line => line.trim());
-	var cellArray = columnHeaders.split(`<`);
+	var cellArray = cells.split(`<`);
 	cellArray.map(line => line.trim());
-	
+
 	var columns = "";
+	var addAtt = '';
+	var counter = 0;
 	for (var i =1; i<headerArray.length; i++) {
 		var addColumn = newColumn;
-		
+
 		var att = headerArray[i].split("/n")
+		console.log("att")
+		console.log(att)
 		for (var j=0; j<att.length; j++) {
 			var row = att[j];
-			if (row.indexOf(`data:text="`)) {
+			if (row.indexOf(`data:text="`) >= 0) {
 			    	text = row.substring(
 					    row.lastIndexOf(`data:text="`) + 11, 
 					    row.indexOf(`"`)
 					).trim();
-			    } else if (row.indexOf(`data:field="`)) {
+			    } else if (row.indexOf(`data:field="`) >=0) {
 				       	sort = row.substring(
 					    row.lastIndexOf(`data:field="`) + 12, 
 					    row.indexOf(`"`)
 					).trim();
 			    } else {
-			    	att+=row+'\n';
+			    	addAtt+=row+'\n';
 			    }
 			
 		}
 		var sort = "";
 		var text = "";
 		
-
-		addColumn.replace("sortPropertyPlaceholder", sort)
-		addColumn.replace("textPlaceholder", text)
-		addColumn.replace("attPlaceholder", att)
-		addColumn.replace("templatePlaceholder", `<` + cells[i])
+		if (!(sort)) {
+			sort = "noLabel" + counter;
+			counter++;
+		}
+		addColumn = addColumn.replace("sortPropertyPlaceholder", sort)
+		if (text) {
+			text = (`<m:Label text="textPropertyPlaceholder" />`).replace("textPropertyPlaceholder", text)
+		}
+		addColumn = addColumn.replace("textPropertyPlaceholder", text)
+		addColumn = addColumn.replace("attPlaceholder", addAtt)
+		addColumn = addColumn.replace("templatePlaceholder", `<` + cellArray[i])
 		columns+=addColumn;
 	}
 		
-	
-	newTable.replace("columnPlaceholder", columns);
+	newTable = newTable.replace("noDataPlaceholder", noData)
+	newTable = newTable.replace("columnPlaceholder", columns);
   
   document.getElementById("text3").value = newTable
 }
